b2f58898f0353bdb1a12a50ac7ca95ea1792dba9
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-09-26 11:48:28 +0700

    Add files via upload

---
+++ PemilihanPercobaan225.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PemilihanPercobaan225 {
+    public static void main (String[] args) {
+        Scanner input25 = new Scanner(System.in);
+        System.out.print("Nilai uas    : ");
+        float uas = input25.nextFloat();
+        System.out.print("Nilai uts    : ");
+        float uts = input25.nextFloat();
+        System.out.print("Nilai kuis   : ");
+        float kuis = input25.nextFloat();
+        System.out.print("Nilai tugas  : ");
+        float tugas = input25.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        System.out.println("Nilai akhir =" + total + "sehingga" + message);
+        
+    }
+}
