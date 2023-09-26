ff9b1361a5b99d516634187b5d1c1375b6e9bbdc
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-09-26 12:17:55 +0700

    Add files via upload

---
+++ PemilihanPercobaan125_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PemilihanPercobaan125{
+    public static void main (String[] args) {
+       Scanner input25 = new Scanner(System.in); 
+       System.out.print("Masukan angka: ");
+       int umur = input25.nextInt();
+        
+       if (umur > 17 ){System.out.println("Umur "+umur+"Bisa menikah");
+
+       }
+           
+       else{ System.out.println("Umur "+umur+"Belum dianjurkan menikah");
+
+       }
+           
+
+    }
+}
+    
